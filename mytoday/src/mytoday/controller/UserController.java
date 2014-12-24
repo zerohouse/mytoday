fd5eae0694254f40d10331d21e50cd0c50ac786a
parksungho <parksungho86@gmail.com>
2014-12-24 21:55:26 +0900

    set

--- mytoday/src/mytoday/controller/UserController.java
+++ mytoday/src/mytoday/controller/UserController.java
@@ -4,18 +4,17 @@ import mytoday.object.Result;
 import mytoday.object.User;
 import easyjdbc.dao.DBMethods;
 import easymapping.annotation.Controller;
-import easymapping.annotation.Get;
+import easymapping.annotation.Post;
 import easymapping.mapping.Http;
 import easymapping.response.Json;
 import easymapping.response.Response;
 
 @Controller
 public class UserController {
-
-	@Get("/users/login.my")
+	@Post("/users/login.my")
 	public Response login(Http http) {
 		User userpassed = http.getJsonObject(User.class, "user");
-		if(userpassed ==null)
+		if (userpassed == null)
 			return new Json(new Result(false, "허용되지 않은 접근입니다."));
 		User fromDB = DBMethods.get(User.class, userpassed.getId());
 		if (fromDB == null)
