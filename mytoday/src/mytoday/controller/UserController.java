f6f611633bb607c25abf9ba6632364510074318f
parksungho <parksungho86@gmail.com>
2014-12-26 03:54:44 +0900

    s

--- mytoday/src/mytoday/controller/UserController.java
+++ mytoday/src/mytoday/controller/UserController.java
@@ -1,9 +1,12 @@
 package mytoday.controller;
 
+import java.io.IOException;
+
 import mytoday.object.Result;
 import mytoday.object.User;
 import easyjdbc.dao.DBMethods;
 import easymapping.annotation.Controller;
+import easymapping.annotation.Get;
 import easymapping.annotation.Post;
 import easymapping.mapping.Http;
 import easymapping.response.Json;
@@ -11,7 +14,16 @@ import easymapping.response.Response;
 
 @Controller
 public class UserController {
-	@Post("/users/login.my")
+	
+	@Get("/users/logout.my")
+	public Response logout(Http http) throws IOException{
+		http.removeSessionAttribute("user");
+		http.write("s");
+		return new Json("s");
+		
+	}
+	
+	@Get("/users/{}.my")
 	public Response login(Http http) {
 		User userpassed = http.getJsonObject(User.class, "user");
 		if (userpassed == null)
@@ -21,6 +33,7 @@ public class UserController {
 			return new Json(new Result(false, "없는 사용자 입니다."));
 		if (!fromDB.isPasswordCorrect(userpassed))
 			return new Json(new Result(false, "패스워드가 틀렸습니다."));
+		http.setSessionAttribute("user", fromDB);
 		return new Json(new Result(true, null));
 	}
 }
