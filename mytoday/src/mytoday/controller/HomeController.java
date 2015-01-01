1aff4d6c12fc144757cedd7038008e195e54ee66
parksungho <parksungho86@gmail.com>
2015-01-01 14:47:28 +0900

    good

--- mytoday/src/mytoday/controller/HomeController.java
+++ mytoday/src/mytoday/controller/HomeController.java
@@ -1,5 +1,20 @@
 package mytoday.controller;
 
-public class HomeController {
+import mytoday.object.User;
+import easymapping.annotation.Controller;
+import easymapping.annotation.Get;
+import easymapping.mapping.Http;
 
+@Controller
+public class HomeController {
+	
+	@Get("/index.my")
+	public void home(Http http){
+		User user = http.getSessionAttribute(User.class, "user");
+		if (user == null){
+			http.sendRedirect("/users/login.my");
+			return;
+			}
+		http.sendRedirect("/mytoday.my");
+	}
 }
--- mytoday/webapp/index.jsp
+++ mytoday/webapp/index.jsp
@@ -2,11 +2,4 @@
 	pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
-<c:choose>
-	<c:when test="${empty sessionScope.user}">
-		<c:redirect url="/users/login.my" />
-	</c:when>
-	<c:otherwise>
-		<c:redirect url="/mytoday.my" />
-	</c:otherwise>
-</c:choose>
+<c:redirect url="/index.my" />
